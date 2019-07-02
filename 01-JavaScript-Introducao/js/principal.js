c613225810f431a619941898205f36315511f81c
Khayan Malantrucco <khayanmalantrucco@gmail.com>
2019-07-02 15:27:22 -0300

    Selecting DOM elements. First validations (if/else).

--- 01-JavaScript-Introducao/js/principal.js
+++ 01-JavaScript-Introducao/js/principal.js
@@ -1,5 +1,43 @@
 //var ola = "Hello World! <3";
 //alert(ola);
 
-var titulo = document.querySelector(".titulo");
+// Alterando um elemento na DOM - h1 através da classe 'titulo'
+var titulo = document.querySelector('.titulo');
 titulo.textContent = "Mariana Nutricionista";
+
+// Selecionando mais elementos na DOM
+var paciente = document.querySelector('.paciente');
+
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+// Flags para validação
+var pesoValido = true;
+var alturaValida = true;
+
+// Seleção do campo IMC
+var tdImc = paciente.querySelector(".info-imc");
+
+// Validação de peso
+if(peso <= 0 || peso >= 500){
+    // console.log("Peso inválido.");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+// Validação de altura
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida.");
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+// Validação de peso e altura. Inserindo o resultado do IMC na DOM
+if(pesoValido && alturaValida){
+    var imc = peso/(altura * 2);
+    tdImc.textContent = imc;
+}
+
