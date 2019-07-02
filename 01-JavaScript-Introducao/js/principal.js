b9b6a0e5556fc439b6ad48ef9babf21d9e0b2730
Khayan Malantrucco <khayanmalantrucco@gmail.com>
2019-07-02 15:50:11 -0300

    Implemented a simple loop for the array 'pacientes'.

--- 01-JavaScript-Introducao/js/principal.js
+++ 01-JavaScript-Introducao/js/principal.js
@@ -5,39 +5,44 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Mariana Nutricionista";
 
-// Selecionando mais elementos na DOM
-var paciente = document.querySelector('.paciente');
+// Criando um Array de elementos da DOM
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
-
-// Flags para validação
-var pesoValido = true;
-var alturaValida = true;
-
-// Seleção do campo IMC
-var tdImc = paciente.querySelector(".info-imc");
-
-// Validação de peso
-if(peso <= 0 || peso >= 500){
-    // console.log("Peso inválido.");
-    pesoValido = false;
-    tdImc.textContent = "Peso inválido!";
+// Percorrendo todo o Array
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    
+    // Flags para validação
+    var pesoValido = true;
+    var alturaValida = true;
+    
+    // Seleção do campo IMC
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    // Validação de peso
+    if (peso <= 0 || peso >= 500) {
+        // console.log("Peso inválido.");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
+    
+    // Validação de altura
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida.");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
+    
+    // Validação de peso e altura. Inserindo o resultado do IMC na DOM
+    if (pesoValido && alturaValida) {
+        var imc = peso/(altura * 2);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
-
-// Validação de altura
-if(altura <= 0 || altura >= 3){
-    console.log("Altura inválida.");
-    alturaValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
-
-// Validação de peso e altura. Inserindo o resultado do IMC na DOM
-if(pesoValido && alturaValida){
-    var imc = peso/(altura * 2);
-    tdImc.textContent = imc;
-}
-
