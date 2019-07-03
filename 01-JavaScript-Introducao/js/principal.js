644ed2c9c3081986e4b9fe4108dd333675e73d6c
Khayan Malantrucco <khayanmalantrucco@gmail.com>
2019-07-03 11:11:03 -0300

    Added a form and an event.

--- 01-JavaScript-Introducao/js/principal.js
+++ 01-JavaScript-Introducao/js/principal.js
@@ -31,6 +31,7 @@ for (var i = 0; i < pacientes.length; i++) {
         // console.log("Peso inválido.");
         pesoValido = false;
         tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
     }
     
     // Validação de altura
@@ -38,6 +39,7 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("Altura inválida.");
         alturaValida = false;
         tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
     }
     
     // Validação de peso e altura. Inserindo o resultado do IMC na DOM
@@ -46,3 +48,40 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+// Adicionando evento no botão
+var btnAdicionar = document.querySelector("#add-paciente");
+btnAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#add-form");
+
+    // Capturando os conteúdos dos campos no form através dos atributos name
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    // Criando o modelo de linha para inserir as informações de um paciente
+    var pacienteTr = document.createElement("tr"); // linha (tr - table roll)
+
+    var nomeTd = document.createElement("td"); // célula (td - table data)
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome; // atribuindo valores capturados às novas células
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd); // inserindo células à nova linha
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr); // inserindo a nova linha à tabela existente
+})
